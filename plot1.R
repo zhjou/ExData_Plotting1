60b75f5b932b671768e360ff6b52de9c516117fc
zhjou <862343996@qq.com>
2015-11-08 23:26:55 +0800

    first add

---
+++ plot1.R
@@ -0,0 +1,5 @@
+colname = read.table("household_power_consumption.txt",sep = ";", nrows = 1,header = TRUE)
+data = read.table("household_power_consumption.txt",sep=";",skip=66637,nrows = 2880,col.names = names(colname))
+png(filename = "plot1.png",width = 480, height = 480, units = "px")
+hist(data$Global_active_power,main = "Global Active Power",xlab = "Global Active Power (kilowatts)",col = "red")
+dev.off()
